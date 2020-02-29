be5bd006d756965f514b7bba68ac9a5912f2c0fa
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-02-28 17:30:40 -0800

    Duplicate todo list App.js.

---
+++ toDoApp.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { Alert, FlatList, ScrollView, StyleSheet, Text, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import Header from './toDoComponents/Header';
+import Todos from './toDoComponents/Todos';
+import AddToDo from './toDoComponents/AddToDo';
+
+
+export default function App() {
+  const[todos, setTodos] = useState([
+    {text: 'get groceries', key: '1'},
+    {text: 'walk dog', key: '2'},
+    {text: 'sleep', key: '3'},
+    {text: 'eat', key: '4'},
+  ])
+  /* this removes a todo from the list when pressed by filtering it from the array*/
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key)
+    })
+  }
+
+  const submitHandler = (text, setText) => {
+
+    if(text.length > 2) {
+      setTodos((prevTodos) => {
+        return [
+          /* Ideally never use Math.random to give a key/id value, download another library to handle this*/
+          {text: text, key: Math.random().toString() },
+          ...prevTodos
+        ]
+      })
+    } else {
+      Alert.alert('OOOPS!', 'Todos must be more than 3 chars long', [
+        {text:'Gotcha'}
+      ])
+    }
+  }
+
+  return (
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+    }}>
+    <View style={styles.container}>
+    <Header/>
+    <View style={styles.content}>
+    <AddToDo submitHandler={submitHandler}/>
+    <View style={styles.list}>
+    <FlatList
+    data={todos}
+    renderItem={({item}) => (
+      <Todos item={item} pressHandler={pressHandler}/>
+    )}
+    />
+    </View>
+    </View>
+    </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  content: {
+    padding: 40,
+    // backgroundColor: 'lightblue',
+    flex: 1,
+  },
+  list: {
+    marginTop: 20,
+    // backgroundColor: 'pink',
+    flex: 1,
+  }
+});
