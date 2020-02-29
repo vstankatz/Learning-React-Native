605a636f700460e699bc96d3502a145b1fbd6b5e
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-02-28 16:30:49 -0800

    Did a lot on this page, first hooks created are now on commentedOutStuff.js.

--- App.js
+++ App.js
@@ -1,80 +1,60 @@
 import React, { useState } from 'react';
-import { ScrollView, StyleSheet, Text, View, Button, TextInput, FlatList, TouchableOpacity } from 'react-native';
+import { Alert, FlatList, ScrollView, StyleSheet, Text, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import Header from './components/Header';
+import Todos from './components/Todos';
+import AddToDo from './components/AddToDo';
+
 
 export default function App() {
-  const [color, setColor] = useState('red');
-  const [name, setName] = useState('roni tsunami');
-
-  const [person, setPerson] = useState({ name: 'Jazzy', age: 6})
-
-  const pressHandler = () => {
-    setName('Veronica');
-    setPerson({ name: 'DJ Jazzy Jeff', age: 6});
-  }
-
-  const [choices, setChoices] = useState([
-    {food: 'ice cream', key: '1'},
-    {food: 'fries', key: '2'},
-    {food: 'veggie burger', key: '3'},
-    {food: 'milkshake', key: '4'},
-    {food: 'onion rings', key: '5'},
+  const[todos, setTodos] = useState([
+    {text: 'get groceries', key: '1'},
+    {text: 'walk dog', key: '2'},
+    {text: 'sleep', key: '3'},
+    {text: 'eat', key: '4'},
   ])
-
-
-  const touchHandler = (key) => {
-    console.log(key);
-    setChoices((prevChoices) => {
-      return prevChoices.filter(choice => choice.key != key)
-      console.log(prevChoices);
+  /* this removes a todo from the list when pressed by filtering it from the array*/
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key)
     })
   }
+
+  const submitHandler = (text, setText) => {
+
+    if(text.length > 2) {
+      setTodos((prevTodos) => {
+        return [
+          /* Ideally never use Math.random to give a key/id value, download another library to handle this*/
+          {text: text, key: Math.random().toString() },
+          ...prevTodos
+        ]
+      })
+    } else {
+      Alert.alert('OOOPS!', 'Todos must be more than 3 chars long', [
+        {text:'Gotcha'}
+      ])
+    }
+  }
+
   return (
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+    }}>
     <View style={styles.container}>
-    <Text>Enter Name:</Text>
-    <TextInput
-    style={styles.textInput}
-    placeholder='John Luc Picard'
-    onChangeText={(name) => setName(name)}/>
-    <Text>Enter Pets Name:</Text>
-    <TextInput
-    style={styles.textInput}
-    placeholder='Muffins'
-    onChangeText={(name) => setPerson(name)}/>
-    <Text>Open up App.js to start working on your app!</Text>
-    <Text style={styles.color}>Let's see how this goes {name}</Text>
-    <Text style={styles.color}>My dogs name is {person.name} she is {person.age}</Text>
-    <View style={styles.button}>
-    <Button title='update name' onPress={pressHandler}/>
-    </View>
-    {/* create list with "flat list component" below...
-    * key differences:
-        - all the items don't automatically load onto the screen if its a large list, it will load when you scroll.
-        - automatically looks for key property so you dont need to declare it. If you want to name it other than key(aka id) use the "keyExtractor"
-          example : keyExtractor={(item) => item.id}
-        -You can set up columns easily with "numColumns"
-          example : numColumns={2}
-    */}
+    <Header/>
+    <View style={styles.content}>
+    <AddToDo submitHandler={submitHandler}/>
+    <View style={styles.list}>
     <FlatList
-    numColumns={2}
-    data={choices}
-    renderItem={({ item }) => (
-    <TouchableOpacity onPress={() => touchHandler(item.key)}>
-    <Text style={styles.food}>{item.food}</Text>
-    </TouchableOpacity>
-    )}/>
-
-  {/*  /Most basic way to generate a list is below...*/}
-
-    {/* <ScrollView>
-    // {choices.map((item) => {
-    //   return (
-    //     <View key={item.key}>
-    //     <Text style={styles.food}>{item.food}</Text>
-    //     </View>
-    //   )
-    // })}
-    // </ScrollView>*/}
+    data={todos}
+    renderItem={({item}) => (
+      <Todos item={item} pressHandler={pressHandler}/>
+    )}
+    />
     </View>
+    </View>
+    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -85,25 +65,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  red: {
-    backgroundColor: 'red'
+  content: {
+    padding: 40,
   },
-  button: {
+  list: {
     marginTop: 20,
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: 'black',
-    padding: 8,
-    margin: 10,
-    width: 200,
-    textAlign: 'center',
-  },
-  food: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24,
-    marginHorizontal: 10,
   }
 });
