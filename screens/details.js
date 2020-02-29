91309ba7dc3acd93b57094a1897a748eed22f72d
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-02-28 17:46:29 -0800

    Remove unneeded folders/files, start building review app framework.

---
+++ screens/details.js
@@ -0,0 +1,2 @@
+import React, { useState } from 'react';
+import { Alert, FlatList, ScrollView, StyleSheet, Text, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
---
+++ screens/home.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { Alert, FlatList, ScrollView, StyleSheet, Text, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
+
+export default function Home() {
+  return (
+    <View>
+    <Text>Home Screen</Text>
+    </View
+  )
+}
