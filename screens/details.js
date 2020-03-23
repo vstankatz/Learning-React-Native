b5a54d869b4c2ab5488011c7af3104b5b5bc5069
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-03-23 03:27:17 -0700

    Add episode details.

--- screens/details.js
+++ screens/details.js
@@ -1,2 +1,21 @@
-import React, { useState } from 'react';
-import { Alert, FlatList, ScrollView, StyleSheet, Text, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Keyboard, Button } from 'react-native';
+import {globalStyles} from '../styles/global';
+
+export default function Details({navigation}) {
+
+
+
+  return (
+    <View style={globalStyles.container}>
+      <Text>{navigation.getParam("seriesCode")}</Text>
+      <Text>{navigation.getParam("episodeTitle")}</Text>
+      <Text>
+        season. 
+        {navigation.getParam("season")}, ep.{navigation.getParam("season")}
+      </Text>
+      <Text>{navigation.getParam("rating")}</Text>
+      <Text>{navigation.getParam("body")}</Text>
+    </View>
+  );
+}
