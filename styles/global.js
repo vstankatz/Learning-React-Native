8c1c8d9ae267892e6bab9f7ac529f1c87bc6eadd
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-03-23 03:28:19 -0700

    Add global styles.

---
+++ styles/global.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+  title: {
+    fontFamily: 'handwriting-google',
+    fontSize: 18,
+    color: 'purple',
+  },
+  paragraph: {
+    marginVertical: 8,
+    lineHeight: 20,
+  }
+})
